830f858cbd07f2b448580be155dd11f6a2a6c200
Suman Rai <officeproj111@gmail.com>
2024-02-17 03:19:57 +0545

    Add new Interface for multiple image

--- Master-BLL/Services/Interface/IUploadImageRepository.cs
+++ Master-BLL/Services/Interface/IUploadImageRepository.cs
@@ -10,7 +10,9 @@ namespace Master_BLL.Services.Interface
     public interface IUploadImageRepository
     {
         Task<string> UploadImage(IFormFile file);
+        Task<List<string>> UploadMultipleImage(List<IFormFile> files);
         void DeleteImage(string ImageUrl);
         void UpdateImage(IFormFile file);
+        void UpdateMultipleImage(List<IFormFile> file);
     }
 }
